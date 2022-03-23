4a05e79f05798454aa1e021eb03744913c1638fe
Jake Klassen <jklassendev@gmail.com>
2022-03-23 05:22:15 -0400

    fix netlify setup

--- remix.config.js
+++ remix.config.js
@@ -2,11 +2,11 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
+  serverBuildTarget: "netlify",
   server: "./server.js",
   ignoredRouteFiles: [".*"],
-  devServerBroadcastDelay: 1000,
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
+  // serverBuildPath: ".netlify/functions-internal/server.js",
   // publicPath: "/build/",
 };
